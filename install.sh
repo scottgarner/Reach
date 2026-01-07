c0013939ca0db010f360c4b176423dc5b32693fc
Scott Garner <scott@j38.net>
2026-01-06 22:41:05 -0800

    Update installer script.

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
-pio run -t upload --upload-port /dev/ttyACM0
-pio run -t upload --upload-port /dev/ttyACM1
-pio run -t upload --upload-port /dev/ttyACM2
+#!/bin/bash
+
+PORTS="
+  /dev/reach-port-1-1
+  /dev/reach-port-1-2
+  /dev/reach-port-3-1
+  /dev/reach-port-3-2
+"
+
+for device in $PORTS; do
+  if [ -e "$device" ]; then
+    echo "Updating $device..."
+    screen -S temp -dm $device 115200
+    screen -S temp -p 0 -X stuff "B"
+    sleep 1
+    pio run -t upload --upload-port $device
+    echo "Done."
+  else
+    echo "Skipping $device (not connected)"
+  fi
+done
+
+echo "All boards updated!"
